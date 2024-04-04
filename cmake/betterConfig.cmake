354ae05f0ed7b6fbd517a4cceee1b2ea790b57a0
DaemonTsun <96687758+DaemonTsun@users.noreply.github.com>
2024-04-04 23:04:33 +0200

    Add -Wno-builtin-declaration-mismatch to gcc warnings

--- cmake/betterConfig.cmake
+++ cmake/betterConfig.cmake
@@ -544,7 +544,7 @@ macro(get_cpp_warnings OUT_VAR)
 
     if (TARGET_CPP_WARNINGS_SANE)
         if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-            list(APPEND ${OUT_VAR} -Wno-unused-but-set-variable -Wno-multichar)
+            list(APPEND ${OUT_VAR} -Wno-unused-but-set-variable -Wno-multichar -Wno-builtin-declaration-mismatch)
         elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
             list(APPEND ${OUT_VAR} -Wno-unused-but-set-variable -Wno-multichar -Wno-missing-braces)
         elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
